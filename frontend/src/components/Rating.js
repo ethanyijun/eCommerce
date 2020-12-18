6e80a3e450342359ee1eecd2dab1b177e384348f
yijungai <ethan.yijun@gmail.com>
2020-12-19 10:40:13 +1100

    fix index and add styling

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,22 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Rating = ({ value, text }) => {
+
+const Rating = ({ value, text, color }) => {
     var floor = Math.floor(value);
     var max = 5;
     var ceil = Math.ceil(value);
     var difference = ceil - floor;
     var rows = [];
+    var index = 0;
 
     for (var m = 0; m < floor; m++) {
-        rows.push(<i class="fas fa-star"></i>);
+        rows.push(<i key={index} style={{color}} className="fas fa-star"></i>);
+        index += 1;
     }
 
     if (difference > 0) {
-        rows.push(<i class="fas fa-star-half-alt"></i>);
+        rows.push(<i key={index} style={{color}} className="fas fa-star-half-alt"></i>);
+        index += 1;
     }
 
-    for (var n = 0; n < max - floor - difference; n++) {
-        rows.push(<i class="far fa-star"></i>);
+    for (var n = 0; n  < max - floor - difference; n++) {
+        rows.push(<i key={index} style={{color}} className="far fa-star"></i>);
+        index += 1;
     }
 
 
@@ -25,8 +31,21 @@ const Rating = ({ value, text }) => {
             <span>
             {rows}
             </span>
+            <span>
+            {text && text}
+            </span>
         </div>
     )
+}
+
+Rating.defaultProps = {
+    color: '#f8e825'
+}
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
 }
 
 export default Rating
