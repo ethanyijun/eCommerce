fa012a8122e7a4d16ef754008aa71664e53f83bf
yijungai <ethan.yijun@gmail.com>
2020-12-27 11:27:48 +1100

    Created products routes and refactor code

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
-import products from './data/products.js'
+import productsRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -14,14 +14,7 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.send(JSON.stringify(products))
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productsRoutes)
 
 const PORT = process.env.PORT || 5000
 
