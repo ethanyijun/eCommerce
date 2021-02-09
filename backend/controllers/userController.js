ad1a8a3c45192d213d7317832d0cea75bcb42d96
yijungai <ethan.yijun@gmail.com>
2021-02-09 23:14:31 +1100

    Add update user profile controller&route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -42,6 +42,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Update user profile
+// @route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("User not found");
+  }
+});
+
 // @desc   Register a new User
 // @route  POST /api/users
 // @access Public
@@ -72,4 +97,4 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, createUser };
+export { authUser, getUserProfile, createUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUser,
   getUserProfile,
   createUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route("/").post(createUser);
 
 export default router;
