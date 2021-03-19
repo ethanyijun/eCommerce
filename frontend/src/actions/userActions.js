f505d091af75bebdbb1c1b4e4874946fb438e8c5
yijungai <ethan.yijun@gmail.com>
2021-03-19 13:46:39 +1100

    Fix log out issue

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -12,8 +12,9 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_RESET,
+  USER_DETAILS_RESET,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
@@ -44,6 +45,8 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => async (dispatch) => {
   dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: ORDER_LIST_MY_RESET });
   localStorage.removeItem("userInfo");
 };
 
