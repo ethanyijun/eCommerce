640fb6669f4253f2331d73e98704bb4601eee30a
yijungai <ethan.yijun@gmail.com>
2021-03-22 12:01:02 +1100

    Add get user and update user backend logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,8 @@ import {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -16,6 +18,9 @@ router
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
 router.route("/").post(createUser).get(protect, admin, getUsers);
-router.route("/:id").delete(protect, admin, deleteUser);
-
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 export default router;
