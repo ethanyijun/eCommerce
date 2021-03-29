c3e9101e7c3e528334d4dd6ef153a8c20071bfec
yijungai <ethan.yijun@gmail.com>
2021-03-29 18:30:55 +1100

    Create and update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,4 +36,60 @@ const deleteProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProductById };
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+  const createdOrder = await product.save();
+  res.status(201).json(createdOrder);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+    const updatedOrder = await product.save();
+    res.status(201).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProductById,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,14 +4,18 @@ import {
   getProductById,
   getProducts,
   deleteProductById,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts);
+router.route("/").post(protect, admin, createProduct);
 
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, admin, deleteProductById);
+  .delete(protect, admin, deleteProductById)
+  .put(protect, admin, updateProduct);
 
 export default router;
