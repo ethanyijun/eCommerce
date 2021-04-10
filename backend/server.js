af0ca9d608e78ccdd356b3666edbb241bd4d9db0
yijungai <ethan.yijun@gmail.com>
2021-04-10 14:28:44 +1000

    add morgan middleware

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,17 @@ import adminRoutes from "./routes/AdminRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import path from "path";
+import morgan from "morgan";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
 
